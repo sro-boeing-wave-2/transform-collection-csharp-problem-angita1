3d611aaf90286b1f25d20234ce5faf3ec8f8eee6
Yaiphabee <ksh.yaipha@gmail.com>
2018-07-20 12:07:22 +0530

    Add transform collection

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -6,9 +6,16 @@ namespace Transform
 {
     public static class Transform 
     {
-        public static void Map()
+        public static List<int> Map(this int[] num,Func<int,int> Double)
         {
+            List<int> newNum = new List<int>();
+            foreach (int item in num)
+            {
+                newNum.Add(Double(item));
+            }   
+            return newNum;
             throw new NotImplementedException();
+
         }
     }
 }
